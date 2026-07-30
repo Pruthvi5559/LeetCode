674b5f3eb83e329670e205fe2398a405daa5cd82
Pruthvi <prithvirajguttedar@gmail.com>
2026-07-30 09:26:36 +0530

    LC3014

---
+++ 2POTDjul2026/4730.cpp
@@ -0,0 +1,27 @@
+//3014. Minimum Number of Pushes to Type Word I
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<int, int> mpp;
+        int result = 0;
+        int j = 2;
+
+        // Assign each character to a keypad button
+        for (char &ch : word) {
+
+            // After button 9, start again from button 2
+            if (j > 9) j = 2;
+
+            // Increase the number of letters on this button
+            mpp[j]++;
+
+            // Pushes required = position of the letter on the button
+            result += mpp[j];
+
+            // Move to the next button
+            j++;
+        }
+
+        return result;
+    }
+};
