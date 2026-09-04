d42b0f5631324095840f128e4a42e52cb3a7f311
Pruthvi <prithvirajguttedar@gmail.com>
2026-09-04 11:10:10 +0530

    LC3903

---
+++ 4POTDsep2026/8104.cpp
@@ -0,0 +1,51 @@
+//3903. Smallest Stable Index I
+/*
+You are given an integer array nums of length n and an integer k.
+
+For each index i, define its instability score as max(nums[0..i]) - min(nums[i..n - 1]).
+
+In other words:
+
+max(nums[0..i]) is the largest value among the elements from index 0 to index i.
+min(nums[i..n - 1]) is the smallest value among the elements from index i to index n - 1.
+An index i is called stable if its instability score is less than or equal to k.
+
+Return the smallest stable index. If no such index exists, return -1.
+
+*/
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> prefixMax(n);
+        vector<int> suffixMin(n);
+
+        // Store the maximum value from index 0 to i.
+        prefixMax[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = max(prefixMax[i - 1], nums[i]);
+        }
+
+        // Store the minimum value from index i to n - 1.
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(suffixMin[i + 1], nums[i]);
+        }
+
+        // Find the smallest stable index.
+        for (int i = 0; i < n; i++) {
+            int instabilityScore = prefixMax[i] - suffixMin[i];
+
+            if (instabilityScore <= k) {
+                return i;
+            }
+        }
+
+        // No stable index exists.
+        return -1;
+    }
+};
